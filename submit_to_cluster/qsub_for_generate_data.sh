b9e5786603aa0f7b8b6b739981d0ad6ac1fd01aa
Linh Hoang <linh.hoang.22@ucl.ac.uk>
2023-09-05 18:22:49 +0100

    edit2

---
+++ submit_to_cluster/qsub_for_generate_data.sh
@@ -0,0 +1,18 @@
+#$ -l tmem=2G
+#$ -l h_vmem=2G
+#$ -l h_rt=06:00:00
+#$ -pe smp 8
+#$ -R y
+
+#These are optional flags but you probably want them in all jobs
+
+#$ -S /bin/bash
+#$ -j y
+#$ -N "generate_data"
+
+#$ -cwd
+
+#The code you want to run now goes here.
+hostname
+date
+python3 -u Simulation_code/run_test/execute_generate_data.py
