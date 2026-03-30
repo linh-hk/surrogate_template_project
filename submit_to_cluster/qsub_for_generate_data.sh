043bd3a948931b03efe8b6563bb023ca15da01ff
Linh Hoang <hoang.khanhlinh3010@gmail.com>
2026-03-30 14:35:18 +0700

    Forgot  in qsub script

--- submit_to_cluster/qsub_for_generate_data.sh
+++ submit_to_cluster/qsub_for_generate_data.sh
@@ -1,6 +1,6 @@
 #$ -l tmem=2G
 #$ -l h_vmem=2G
-#$ -l h_rt=06:00:00
+#$ -l h_rt=10:00:00
 #$ -pe smp 8
 #$ -R y
 
@@ -15,4 +15,4 @@
 #The code you want to run now goes here.
 hostname
 date
-python3 -u Simulation_code/run_test/execute_generate_data.py
+python3 -u Simulation_code/run_test/execute_generate_data.py $1
